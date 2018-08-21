342ecc4448e0248567b019cc2a53f5c3ad4fe1f9
TUPOBO <1028461931@qq.com>
2018-08-21 09:58:03 +0800

    获取图书详情并修改详情页标题显示

--- src/pages/detail/index.vue
+++ src/pages/detail/index.vue
@@ -10,12 +10,16 @@ import { get } from '@/utils'
 export default {
   data () {
     return {
-      bookId: ''
+      bookId: '',
+      info: {}
     }
   },
   methods: {
     async getDetail () {
       const info = await get('/weapp/bookdetail', {id: this.bookId})
+      wx.setNavigationBarTitle({
+        title: `《 ${info.data.title} 》`
+      })
       console.log(info)
     }
   },
