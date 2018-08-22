5c7fafe3f9c6e980b010c1a00e7bfb1ef8c11348
TUPOBO <1028461931@qq.com>
2018-08-22 17:17:56 +0800

    评论页显示当前用户添加的书

--- src/pages/comments/index.vue
+++ src/pages/comments/index.vue
@@ -1,7 +1,14 @@
 <template>
   <div>
     <div class="container">
-      <CommentList type="user" :comments="comments"></CommentList>
+      <CommentList vif="userInfo.openId" type="user" :comments="comments"></CommentList>
+      <div v-if="userInfo.openId">
+        <div class="pageTitle">我添加的图书：</div>
+        <card :book="book" v-for="(book, index) in books" :key="index"></card>
+      </div>
+      <div v-if="!books.length">
+        暂未添加图书，请去个人主页扫码添加图书
+      </div>
     </div>
   </div>
 </template>
@@ -9,28 +16,42 @@
 <script>
 import { get } from '@/utils'
 import CommentList from '@/components/CommentList'
+import card from '@/components/card'
 export default {
   data () {
     return {
       comments: [],
-      userInfo: {}
+      userInfo: {},
+      books: []
     }
   },
   components: {
-    CommentList
+    CommentList,
+    card
   },
   methods: {
     init () {
       wx.showNavigationBarLoading()
       this.getComments()
+      this.getBooks()
+      wx.hideNavigationBarLoading()
     },
     async getComments () {
       const comments = await get('/weapp/commentlist', {
         openid: this.userInfo.openId
       })
       this.comments = comments.data.list
-      wx.hideNavigationBarLoading()
+    },
+    async getBooks () {
+      const books = await get('/weapp/booklist', {
+        openid: this.userInfo.openId
+      })
+      this.books = books.data.list
     }
+  },
+  onPullDownRefresh () {
+    this.init()
+    wx.stopPullDownRefresh()
   },
   onShow () {
     if (!this.userInfo.openId) {
@@ -46,5 +67,10 @@ export default {
 </script>
 
 <style scoped>
-
+  .pageTitle {
+    padding-left: 20px;
+    background: #eee;
+    line-height: 40px;
+    font-size: 14px;
+  }
 </style>
